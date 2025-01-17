8a56c52776ba9965a50c6e7b69b7fe5c9413783b
MineItRiGrEight <80468882+mirlge@users.noreply.github.com>
2025-01-17 20:39:18 +0100

    feat(which-key): Add partial WhichKey integration for Normal mode `<A-[ai]>` keybinds

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,12 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [Unreleased]
+
+### Added
+
+- Partial WhichKey integration for Normal mode `<A-[ai]>` keybinds
+
 ## [6.3.2] - 2025-01-17
 
 ### Fixed
--- lua/kak/which-key-integration.lua
+++ lua/kak/which-key-integration.lua
@@ -1,5 +1,8 @@
 local function text_objects()
-  for _, keybind in ipairs(require("which-key.plugins.presets").text_objects) do
+  local wk_text_objects = require("which-key.plugins.presets").text_objects
+  table.insert(wk_text_objects.mode, "n")
+
+  for _, keybind in ipairs(wk_text_objects) do
     local keybind_str = keybind[1]
     if string.len(keybind_str) == 1 then
       keybind[1] = "<A-" .. keybind_str .. ">"
